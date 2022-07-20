50316494df88cebfe5d6b2b9ef137b8d177a0add
wangxueqing <wangxueqing03@kuaishou.com>
2022-07-20 10:20:40 +0800

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+//正常版本
+import {TodoList} from './component/todo';
+//bug版本
+// import { TodoList } from './components/todo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <h1>Todo List</h1>
+      <TodoList></TodoList>
+    </div>
     </div>
   );
 }
